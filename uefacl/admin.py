0dea68abc425600c85423774c9af8e3d70572c53
YaGiNA <yutachihaya@gmail.com>
2016-11-01 23:54:36 +0900

    Copy from worldcup/ dir.

--- uefacl/admin.py
+++ uefacl/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Nation, Game
+
+
+class NationAdmin(admin.ModelAdmin):
+    list_display = ('nation_name', 'points',
+    'games', 'wins', 'loses', 'draws', 'percentage',
+    'get_goal', 'lost_goal', 'goal_diff')
+
+
+admin.site.register(Nation, NationAdmin)
+admin.site.register(Game)
--- uefacl/models.py
+++ uefacl/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Nation(models.Model):
+
+    class Meta:
+        verbose_name = 'チーム'
+        verbose_name_plural = 'チームら'
+        ordering = ['-points', '-goal_diff', '-get_goal']
+        # priority: points -> goal_diff -> get_goal
+
+
+    standings = models.IntegerField(default=0)
+    nation_name = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+    games = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    draws = models.IntegerField(default=0)
+    percentage = models.FloatField(default=0.000)
+    get_goal = models.IntegerField(default=0)
+    lost_goal = models.IntegerField(default=0)
+    goal_diff = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nation_name
+
+
+class Game(models.Model):
+
+    class Meta:
+        verbose_name = '試合'
+        verbose_name_plural = '試合'
+
+    team = models.ForeignKey(Nation, related_name='team', default="")
+    team_score = models.IntegerField(default=0)
+    opposite = models.ForeignKey(Nation, related_name='oppo', default="")
+    opposite_score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.team) + " vs. " + str(self.opposite)
