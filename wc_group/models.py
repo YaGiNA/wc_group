91854a266a01dc2524e8d2c3d31c03e08de11a62
YaGiNA <yutachihaya@gmail.com>
2016-11-01 13:18:20 +0900

    Add game class

--- wc_group/models.py
+++ wc_group/models.py
@@ -7,3 +7,9 @@ class Nation(models.Model):
     draws =       models.IntegerField(default=0)
     get_points =  models.IntegerField(default=0)
     lose_points = models.IntegerField(default=0)
+    # 得失点差は順位表で計算する
+
+class Game(models.Model):
+    game_card = models.ForeignKey(Nation, Nation)
+    game_score = (models.IntegerField(default=0), models.IntegerField(default=0))
+    # 対戦カード、スコアをタプルで定義する
