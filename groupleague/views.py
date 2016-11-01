bca6a9763555aab2ac34a0be9e8322d61977da73
YaGiNA <yutachihaya@gmail.com>
2016-11-01 20:26:52 +0900

    Add save method

--- groupleague/views.py
+++ groupleague/views.py
@@ -5,28 +5,40 @@ from .models import Nation, Game
 
 def addresult_win(Nation):
     Nation.wins += 1
-    points += 3
+    Nation.points += 3
+    print(Nation.points)
 
 def addresult_lose(Nation):
     Nation.loses += 1
+    print(Nation.points)
+
 
 def addresult_draw(Nation):
     Nation.draws += 1
-    points += 1
+    Nation.points += 1
+    print(Nation.points)
+
 
 def apply_results(Nation, gget, glost):
     Nation.get_goal += gget
     Nation.lost_goal += glost
     Nation.goal_diff = Nation.get_goal - Nation.lost_goal
     if gget > glost:
-        addresult_win()
+        addresult_win(Nation)
     elif gget == glost:
-        addresult_draw()
+        addresult_draw(Nation)
     else:
-        addresult_lose()
+        addresult_lose(Nation)
+    Nation.save()
+
 
 
 def index(request):
+    games = Game.objects.all()
+    for game in games:
+        apply_results(game.team, game.team_score, game.opposite_score)
+
+
     return render(request, 'groupleague/index.html', {
         'nations': Nation.objects.all(),
     })
