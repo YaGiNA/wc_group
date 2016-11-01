a36ecca9dcedd54d083562d51c94bfae7e507c0f
YaGiNA <yutachihaya@gmail.com>
2016-11-01 13:12:57 +0900

    Add Nation class

--- wc_group/models.py
+++ wc_group/models.py
@@ -1,1 +1,9 @@
  from django.db import models
+
+class Nations(models.Model):
+    nation_name = models.CharField(max_length=200)
+    wins =        models.IntegerField(default=0)
+    loses =       models.IntegerField(default=0)
+    draws =       models.IntegerField(default=0)
+    get_points =  models.IntegerField(default=0)
+    lose_points = models.IntegerField(default=0)
