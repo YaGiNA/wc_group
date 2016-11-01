e8dd6eb0422b170b67f58797588659dac64d4db9
YaGiNA <yutachihaya@gmail.com>
2016-11-01 23:27:40 +0900

    Add admin items

--- worldcup/admin.py
+++ worldcup/admin.py
@@ -3,18 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Nation, Game
 
-'''
-class GamesInline(admin.StackedInline):
-    model = Game
-    extra = len(Nation.objects.all()) - 1
-    fk_name = 'team'
 
-
-#class NationAdmin(admin.ModelAdmin):
-    inlines = [GamesInline]
+class NationAdmin(admin.ModelAdmin):
+    list_display = ('nation_name', 'points',
+    'games', 'wins', 'loses', 'draws', 'percentage',
+    'get_goal', 'lost_goal', 'goal_diff')
 
 
 admin.site.register(Nation, NationAdmin)
-'''
-admin.site.register(Nation)
 admin.site.register(Game)
